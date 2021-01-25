22551d734df2ba369759442e910ed11b0fd0d5c0
Raúl <58739774+Almorox13@users.noreply.github.com>
2021-01-25 12:56:44 +0100

    update

--- Arduino/ProgramaArduino/ProgramaArduino.ino
+++ Arduino/ProgramaArduino/ProgramaArduino.ino
@@ -1,24 +1,42 @@
-#include <SoftwareSerial.h>
-SoftwareSerial BT1(10, 11); // RX, TX se cruzan
+//Motor 1
+#define RPWM 2 // define pin 3 for RPWM pin (output)
+#define LPWM 3 // define pin 6 for LPWM pin (output)
+#define R_EN 4 // define pin 2 for R_EN pin (input)
+#define L_EN 5 // define pin 7 for L_EN pin (input)
+#define R_IS 50 // define pin 5 for R_IS pin (output)
+#define L_IS 51 // define pin 8 for L_IS pin (output)
 
-int IN1 = 2;
-int IN2 = 3;
-int ENA = 5;
-int IN3 = 7;
-int IN4 = 8;
-int ENB = 9;
-char rec;
+//Motor 2
+#define RPWM2 6 // define pin 3 for RPWM pin (output)
+#define LPWM2 7 // define pin 6 for LPWM pin (output)
+#define R_EN2 8 // define pin 2 for R_EN pin (input)
+#define L_EN2 9 // define pin 7 for L_EN pin (input)
+#define R_IS2 52 // define pin 5 for R_IS pin (output)
+#define L_IS2 53 // define pin 8 for L_IS pin (output)
+
+#define CW 1 //do not change
+#define CCW 0 //do not change
+
+#define debug 1 //change to 0 to hide serial monitor debugging information or set to 1 to view
 bool correcto = false;
+char rec;
 
-void setup()
-{
-  
+#include <RobojaxBTS7960.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial BT1(10, 11); // RX, TX se cruzan
+RobojaxBTS7960 motor(R_EN,RPWM,R_IS, L_EN,LPWM,L_IS,debug);
+RobojaxBTS7960 motor2(R_EN2,RPWM2,R_IS2, L_EN2,LPWM2,L_IS2,debug);
+
+void setup() {
   BT1.begin(9600);
   Serial.begin(9600);
+  motor.begin();
+  motor2.begin();
 }
 
 void loop() {
-
+  
   leer();
 
   if(correcto == true){
@@ -59,34 +77,40 @@ void loop() {
 
 void norte(){
   
-     motor.rotate(100,CW);
+     motor.rotate(20,CCW);
+     motor2.rotate(20,CCW);
 
       delay(100);
 }
 
 void sur(){
   
-    motor.rotate(100,CCW);
+    motor.rotate(20,CW);
+    motor2.rotate(20,CW);
 
       delay(100);
 }
 
 void este(){
   
-      motor.rotate(100,CCW);
+      motor.rotate(20,CCW);
+      motor2.rotate(20,CW);
+
       delay(100);
 }
 
 void oeste(){
 
-      motor.rotate(100,CW);
+      motor.rotate(20,CW);
+      motor2.rotate(20,CCW);
       delay(100);
 }
 
 void parar(){
 
      motor.stop();
-      delay(50);
+     motor2.stop();
+     delay(50);
 }
 
 bool leer(){
